2925e4661490d264cfbcbb753f2c217c9c23224f
Chris Henning <2653277+chennin@users.noreply.github.com>
2023-12-25 14:31:38 -0500

    Reduce image size

--- build-ext.sh
+++ build-ext.sh
@@ -16,7 +16,9 @@ if [ -e stellarisdashboard/parsing/rust_parser ]; then
   esac
   cd stellarisdashboard/parsing/rust_parser
   # https://github.com/PyO3/maturin/issues/284
-  env VIRTUAL_ENV=$(python3 -c 'import sys; print(sys.base_prefix)') maturin develop -r
+  export CARGO_TARGET_DIR="/tmp/target"
+  env VIRTUAL_ENV=$(python3 -c 'import sys; print(sys.base_prefix)') maturin develop --release --strip
+  rm -rf "${CARGO_TARGET_DIR}"
   #rm -rf "$HOME/.cargo/registry"
   rustup self uninstall -y
 fi
